2030157fcce7eeb9881111b50b041a3bd56da5c9
Jan Jędrzejewski <janjedrzejewski@gmail.com>
2024-07-23 18:15:53 +0200

    Refactor - added abstraction for better readability

--- server.c
+++ server.c
@@ -29,43 +29,6 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
-  int listen_value = listen(server_data.socketfd, server_data.backlog);
-  if (listen_value == -1) {
-    printf("ERROR while trying to listen for socket connections!\n errno: %d "
-           "%s\n",
-           errno, strerror(errno));
-    exit(EXIT_FAILURE);
-  }
-
-  // get server address information
-  char hostBuffer[NI_MAXHOST], serviceBuffer[NI_MAXSERV];
-  int name_info_value = getnameinfo(
-      (struct sockaddr *)&server_data.address, sizeof(server_data.address),
-      hostBuffer, sizeof(hostBuffer), serviceBuffer, sizeof(serviceBuffer), 0);
-
-  if (name_info_value != 0) {
-    printf(
-        "ERROR while trying to get server socket information!\n Error: %d %s\n",
-        name_info_value, gai_strerror(name_info_value));
-    exit(EXIT_FAILURE);
-  }
-
-  struct sockaddr_in client_address;
-  unsigned int client_lenght = sizeof(client_address);
-
-  while (1) {
-    int client_socketfd =
-        accept(server_data.socketfd, (struct sockaddr *)&client_address,
-               (socklen_t *)client_lenght);
-
-    if (client_socketfd == -1) {
-      printf("ERROR while accepting client socket!\n errno: %d "
-             "%s\n",
-             errno, strerror(errno));
-      exit(EXIT_FAILURE);
-    }
-  };
-
   return 0;
 }
 
